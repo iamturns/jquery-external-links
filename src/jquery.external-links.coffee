eb21617c3855f80d38f249f07fd2dd5738ee355c
Matthew Turnbull <turnbullm@gmail.com>
2015-02-07 18:54:50 +0000

    Code

---
+++ src/jquery.external-links.coffee
@@ -0,0 +1,32 @@
+window.ExternalLinks = class ExternalLinks
+
+  defaultOptions:
+    internalDomain: window.location.host,
+    setTargetBlank: true,
+    addClass: true,
+    class: 'external',
+    setTitle: true,
+    title: 'Opens new window'
+
+  constructor: (@el, @$el, options = {}) ->
+    @options = $.extend @defaultOptions, options
+    @markAsExternal() if @isExternalUrl()
+
+  isExternalUrl: ->
+    @el.href.indexOf(@options.internalDomain) == -1
+
+  markAsExternal: ->
+    if @options.setTargetBlank then @$el.attr('target', '_blank')
+    if @options.addClass then @$el.addClass(@options.class)
+    if @options.setTitle then @$el.attr('title', @options.title)
+
+# jQuery interface
+
+(($) ->
+  $.fn.externalLinks = (options = {}) ->
+    return @each () ->
+      el = @
+      $el = $(el)
+      newPluginInstance = new window.ExternalLinks(el, $el, options)
+      $el.data('externalLinks', newPluginInstance)
+)(jQuery)
---
+++ src/jquery.external-links.js
@@ -0,0 +1,58 @@
+var ExternalLinks;
+
+window.ExternalLinks = ExternalLinks = (function() {
+  ExternalLinks.prototype.defaultOptions = {
+    internalDomain: window.location.host,
+    setTargetBlank: true,
+    addClass: true,
+    "class": 'external',
+    setTitle: true,
+    title: 'Opens new window'
+  };
+
+  function ExternalLinks(_at_el, _at_$el, options) {
+    this.el = _at_el;
+    this.$el = _at_$el;
+    if (options == null) {
+      options = {};
+    }
+    this.options = $.extend(this.defaultOptions, options);
+    if (this.isExternalUrl()) {
+      this.markAsExternal();
+    }
+  }
+
+  ExternalLinks.prototype.isExternalUrl = function() {
+    return this.el.href.indexOf(this.options.internalDomain) === -1;
+  };
+
+  ExternalLinks.prototype.markAsExternal = function() {
+    if (this.options.setTargetBlank) {
+      this.$el.attr('target', '_blank');
+    }
+    if (this.options.addClass) {
+      this.$el.addClass(this.options["class"]);
+    }
+    if (this.options.setTitle) {
+      return this.$el.attr('title', this.options.title);
+    }
+  };
+
+  return ExternalLinks;
+
+})();
+
+(function($) {
+  return $.fn.externalLinks = function(options) {
+    if (options == null) {
+      options = {};
+    }
+    return this.each(function() {
+      var $el, el, newPluginInstance;
+      el = this;
+      $el = $(el);
+      newPluginInstance = new window.ExternalLinks(el, $el, options);
+      return $el.data('externalLinks', newPluginInstance);
+    });
+  };
+})(jQuery);
